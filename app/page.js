ee566d8f01b7015180463d5504909df0a7c21cd2
aesh-patel22 <aeshashiv81@gmail.com>
2026-09-05 21:09:25 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -205,7 +205,7 @@ export default function Home() {
               <div className="ganesha-glow" />
 
               <img
-                src="/images/ganesha1.webp"
+                src="/images/ganesha2.webp"
                 alt="Ganpati Bappa"
                 className="ganesha-image"
               />
